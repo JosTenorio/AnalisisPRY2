e4390c2ce3b18df797415afbda8cefa1c8ac7fc0
TilapiaKIddo2 <jostenoriop@gmail.com>
2020-06-17 15:30:09 -0600

    Added random bouncing

--- Monte Carlo Path Tracer/Main.py
+++ Monte Carlo Path Tracer/Main.py
@@ -79,7 +79,7 @@ def tracePath(ray, depth):
             # end if ray didnt bounce
             if depth == 0:
                 return 0
-
+            print ("Light source reached")
             x = int(intersection[0])
             y = int(intersection[1])
             distance = intersection[2]
@@ -114,12 +114,12 @@ def tracePath(ray, depth):
             # create a new ray
             if boundary.specular:
                 # SET THE SPECULAR ANGLE BY REPLACING THE 0
-                newRay = Ray(x, y, 0)
+                newRay = Ray(x, y, 10)
             else:
                 # SET A RANDOM VALID ANGLE BY REPLACING THE 0
-                newRay = Ray(x, y, 0)
+                newRay = randomBounce(intersection, boundary, ray)
 
-            # obatin the incoming color
+            # obtain the incoming color
             colorIncoming = tracePath(newRay, depth + 1)
 
             # combine color, light source and light color
@@ -136,7 +136,7 @@ def tracePath(ray, depth):
 WIDTH = 500
 HEIGHT = 500
 RUNNING = True
-NUM_SAMPLES = 0
+NUM_SAMPLES = 50
 MAX_DEPTH = 2
 
 # pygame setup
@@ -180,7 +180,62 @@ def drawBoundaries():
     for line in boundaries:
         line.draw(WINDOW)
 
-# thread setup
+def randomBounce (intersection, Line, ray):
+    try:
+        m = (Line.b[1] - Line.a[1]) / (Line.b[0] - Line.a [0])
+        if m == 0:
+            if ray.pos [0] == intersection [0]:
+                if ray.pos[0] > intersection[0]:
+                    return Ray(intersection[0], intersection[1],random.choice([random.uniform(0, 89), random.uniform(271, 359)]))
+                else:
+                    return Ray(intersection[0], intersection[1], random.uniform(91, 269))
+            else:
+                if ray.pos[1] > intersection [1]:
+                    return Ray(intersection[0], intersection[1],random.uniform(1,179))
+                else:
+                    return Ray(intersection[0], intersection[1],random.uniform(189,359))
+        else:
+            b = Line.b[1] - (Line.b[0] * m)
+            YL = (m * ray.pos[0]) + b
+            DY = YL - ray.pos[1]
+            LineAngle = np.rad2deg(np.arctan(m))
+            if (m > 0) and (DY > 0):
+                return Ray(intersection[0], intersection[1], random.uniform(LineAngle, LineAngle - 180))
+            elif (m > 0) and (DY < 0):
+                return Ray(intersection[0], intersection[1], random.uniform(LineAngle, LineAngle + 180))
+            elif (m < 0) and (DY > 0):
+                return Ray(intersection[0], intersection[1], random.uniform(LineAngle, LineAngle - 180))
+            elif (m < 0) and (DY < 0):
+                return Ray(intersection[0], intersection[1], random.uniform(LineAngle, LineAngle + 180))
+    except ZeroDivisionError:
+        if ray.pos[0] == intersection [0]:
+            if ray.pos[1] > intersection [1]:
+                return Ray(intersection[0], intersection[1],random.uniform(1,179))
+            else:
+                return Ray(intersection[0], intersection[1],random.uniform(189,359))
+        else:
+            if ray.pos[0] > intersection [0]:
+                return Ray (intersection[0],intersection[1], random.choice([random.uniform(0,89),random.uniform(271,359)]))
+            else:
+                return Ray(intersection[0], intersection[1],random.uniform(91,269))
+
+def specularBounce (intersection, line, ray):
+    try:
+        mline = (line.b[1] - line.a[1]) / (line.b[0] - line.a [0])
+        LineAngle = np.rad2deg(np.arctan(mline))
+        newline = Line(intersection[0], intersection[1], ray.pos[0], ray.pos[1])
+        mNewLine = (newline.b[1] - newline.a[1]) / (newline.b[0] - newline.a [0])
+        incidentAngle = math.atan2((mline - mNewLine), 1 + (mNewLine * mline))
+        incidentAngle = np.rad2deg(incidentAngle)
+        #print ("Angulo incidente: ")
+        #print (incidentAngle)
+        result = Ray (intersection[0],intersection[1], LineAngle + incidentAngle)
+        return result
+    except ZeroDivisionError:
+        print ("Ray or segment are vertical")
+
+
+#thread setup
 tracerThread = threading.Thread(target = renderLight, daemon = True)
 tracerThread.start()
 
@@ -199,8 +254,12 @@ while RUNNING:
     surface = py.surfarray.make_surface(drawingPixels)
     WINDOW.blit(surface, (0, 0))
 
-    drawBoundaries()
-
+    # Ray1 = Ray(0,500,360-70)
+    # Ray1.draw(WINDOW)
+    # newLine = Line(50,250,300,250)
+    # newLine.draw(WINDOW)
+    # newRay = specularBounce (Ray1.checkIntersection(newLine), newLine,Ray1)
+    # newRay.draw(WINDOW)
     # update pygame
     py.display.flip()
     CLOCK.tick(60)
